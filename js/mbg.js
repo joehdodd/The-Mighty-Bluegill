20e4fdce01926df508663b99325a77b64eafc862
joehdodd <joehdodd@gmail.com>
2017-02-09 20:12:11 -0500

    Initial Commit
    Website for local Gyotaku artist.

---
+++ js/mbg.js
@@ -0,0 +1,9 @@
+/*var parallaxHeight = $('.jumbotron').outerHeight();
+function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.top-bg').css('height', (parallaxHeight-scrolled) + 'px');
+}
+
+$(window).scroll(function(e){
+    parallax();
+});*/
